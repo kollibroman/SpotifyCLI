0b63e353637c561ce393848aa6c3f9afe35de0b0
Filip <filipkwiek888@gmail.com>
2022-02-13 14:01:35 +0100

    Full fixes

--- src/SpotifyCli.core/Data/App.cs
+++ src/SpotifyCli.core/Data/App.cs
@@ -1,8 +1,8 @@
 namespace SpotifyClientCli.Data
 {
-    public class App
+    public class App //Fuck privacy maybe I will fix that in future
     {
-        public string? ClientId { get; set; } =  File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/ID.txt")!;
-        public string? ClientSecret { get; set; } =  File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Secret.txt")!;
+        public string? ClientId { get; set; } =  "71aa513b5736479a82bf3600a6b849ea";
+        public string? ClientSecret { get; set; } = "67ab8c73822c492e9308eafd8dc2f644";
     }
 }
